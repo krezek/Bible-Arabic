1332c34748310c3cb093c2181c92b48a197926f0
Kinaz Rezek <kinaz_rezek@hotmail.com>
2022-10-02 13:23:25 +0200

    adding winbase project

---
+++ Bible-Arabic/main_wnd.c
@@ -0,0 +1,135 @@
+#include "platform.h"
+
+#include "main_wnd.h"
+#include "resource.h"
+
+static TCHAR szWindowClass[] = _T("DesktopApp");
+static TCHAR szTitle[] = _T("Holy Bible");
+
+static void AfterCreate(BaseWindow* _this);
+static LRESULT HandleMessage(BaseWindow* _this, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+ATOM MainWindow_RegisterClass()
+{
+    WNDCLASSEX wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_DBLCLKS;
+    wcex.lpfnWndProc = BaseWindow_Proc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = GetModuleHandle(NULL);
+    wcex.hIcon = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL; // MAKEINTRESOURCE(IDR_POPUPMENU);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDC_MYICON));
+
+    return RegisterClassEx(&wcex);
+}
+
+BOOL Create(BaseWindow* _this)
+{
+    HWND hWnd = CreateWindow(
+        szWindowClass,
+        szTitle,
+        WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        800, 600,
+        NULL,
+        NULL,
+        GetModuleHandle(NULL),
+        _this
+    );
+
+    _this->_hWnd = hWnd;
+
+    AfterCreate(_this);
+
+    return _this->_hWnd ? TRUE : FALSE;
+}
+
+MainWindow* MainWindow_init()
+{
+    MainWindow* mw = (MainWindow*)malloc(sizeof(MainWindow));
+    assert(mw != NULL);
+
+    BaseWindow_default((BaseWindow*)mw);
+
+    mw->_client_width = mw->_client_height = 0;
+
+    mw->_baseWindow._HandleMessageFunc = HandleMessage;
+    mw->_baseWindow._CreateFunc = Create;
+
+    return mw;
+}
+
+void MainWindow_free(MainWindow* mw)
+{
+    free(mw);
+}
+
+static void OnCreate(MainWindow* mw)
+{
+    RECT rc;
+    GetClientRect(mw->_baseWindow._hWnd, &rc);
+
+    mw->_client_width = rc.right - rc.left;
+    mw->_client_height = rc.bottom - rc.top;
+}
+
+static void OnSize(MainWindow* mw, int width, int height)
+{
+    if (!IsWindowVisible(mw->_baseWindow._hWnd))
+        return;
+}
+
+void AfterCreate(BaseWindow* _this)
+{
+    MainWindow* mw = (MainWindow*)_this;
+}
+
+static void OnPaint(MainWindow* mw)
+{
+    PAINTSTRUCT ps;
+    HDC hdc = BeginPaint(mw->_baseWindow._hWnd, &ps);
+
+    FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOWFRAME));
+
+    EndPaint(mw->_baseWindow._hWnd, &ps);
+}
+
+static void OnDestroy(MainWindow* mw)
+{
+}
+
+static LRESULT HandleMessage(BaseWindow* _this, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    MainWindow* mw = (MainWindow*)_this;
+
+    switch (uMsg)
+    {
+    case WM_CREATE:
+        OnCreate(mw);
+        return 0;
+
+    case WM_PAINT:
+        OnPaint(mw);
+        return 0;
+
+    case WM_SIZING:
+        return mw->_baseWindow._OnSizingFunc(&mw->_baseWindow, (RECT*)lParam);
+
+    case WM_SIZE:
+        OnSize(mw, LOWORD(lParam), HIWORD(lParam));
+        return 0;
+
+    case WM_DESTROY:
+        OnDestroy(mw);
+        return 0;
+
+    default:
+        return DefWindowProc(_this->_hWnd, uMsg, wParam, lParam);
+    }
+}
---
+++ Bible-Arabic/main_wnd.h
@@ -0,0 +1,19 @@
+#ifndef _MAIN_WND_H_
+#define _MAIN_WND_H_
+
+#include "base_wnd.h"
+
+typedef struct
+{
+	BaseWindow _baseWindow;
+
+	int _client_width, _client_height;
+} MainWindow;
+
+ATOM MainWindow_RegisterClass();
+
+MainWindow* MainWindow_init();
+void MainWindow_free(MainWindow* mw);
+
+#endif /* _MAIN_WND_H_ */
+
