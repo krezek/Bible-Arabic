35cf780bc96e6dec19028d7b3d92f8498ade8749
Kinaz Rezek <kinaz_rezek@hotmail.com>
2022-10-07 20:45:19 +0200

    adding more data

---
+++ data/genesis/genesis_23.sql
@@ -0,0 +1,19 @@
+INSERT INTO genesis (chapter, verse, prefix, suffix, body)
+VALUES (1, 1, NULL, NULL, "فِي الْبَدْءِ خَلَقَ اللهُ السَّمَاوَاتِ وَالأَرْضَ.");
+
+INSERT INTO genesis (chapter, verse, prefix, suffix, body)
+VALUES (1, 2, NULL, NULL, "وَكَانَتِ الأَرْضُ خَرِبَةً وَخَالِيَةً، وَعَلَى وَجْهِ الْغَمْرِ ظُلْمَةٌ، وَرُوحُ اللهِ يَرِفُّ عَلَى وَجْهِ الْمِيَاهِ.");
+
+INSERT INTO genesis (chapter, verse, prefix, suffix, body)
+VALUES (1, 3, NULL, NULL, "وَقَالَ اللهُ: «لِيَكُنْ نُورٌ»، فَكَانَ نُورٌ.");
+
+
+
+INSERT INTO genesis (chapter, verse, prefix, suffix, body)
+VALUES (2, 1, NULL, NULL, "فَأُكْمِلَتِ السَّمَاوَاتُ وَالأَرْضُ وَكُلُّ جُنْدِهَا.");
+
+INSERT INTO genesis (chapter, verse, prefix, suffix, body)
+VALUES (2, 2, NULL, NULL, "وَفَرَغَ اللهُ فِي الْيَوْمِ السَّابعِ مِنْ عَمَلِهِ الَّذِي عَمِلَ. فَاسْتَرَاحَ فِي الْيَوْمِ السَّابعِ مِنْ جَمِيعِ عَمَلِهِ الَّذِي عَمِلَ.");
+
+INSERT INTO genesis (chapter, verse, prefix, suffix, body)
+VALUES (2, 3, NULL, NULL, "وَبَارَكَ اللهُ الْيَوْمَ السَّابعَ وَقَدَّسَهُ، لأَنَّهُ فِيهِ اسْتَرَاحَ مِنْ جَمِيعِ عَمَلِهِ الَّذِي عَمِلَ اللهُ خَالِقًا.");
