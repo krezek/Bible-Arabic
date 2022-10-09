0c2b994def82a1370de92d3e473562ce6fdab7c6
Kinaz Rezek <kinaz_rezek@hotmail.com>
2022-10-09 13:39:58 +0200

    complete genesis

---
+++ converter/converter.c
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
+const char* source = "C:\\Users\\Kinaz Rezek\\Test\\ar_new\\01\\50.htm";
+const char* dist = "generated.sql";
+
+#define chapter 50
+
+#define BUFF_LENGTH 1024
+char buffer[BUFF_LENGTH];
+
+int line = 1;
+int idx = 0;
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "en_US.UTF-8");
+
+	FILE* pSrc = fopen(source, "r");
+	FILE* pDist = fopen(dist, "w");
+
+	int c;
+	while ((c = getc(pSrc)) != EOF)
+	{
+		if (c == L'\n' || c == L'\r')
+		{
+			if (c == L'\r')
+				continue;
+
+			buffer[idx] = 0;
+
+			fprintf(pDist, "INSERT INTO genesis (chapter, verse, prefix, suffix, body)\nVALUES (%d, %d, NULL, NULL, \"%s\");\n", chapter, line, strchr(buffer, ';') + 1);
+			
+			++line;
+			idx = 0;
+		}
+		else
+		{
+			buffer[idx++] = c;
+		}
+	}
+
+	fclose(pDist);
+	fclose(pSrc);
+
+	return 0;
+}
