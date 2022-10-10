c1a7ea17e7a7482d15f85ae0d70b0ac0c1d28b94
Kinaz Rezek <kinaz_rezek@hotmail.com>
2022-10-10 17:25:13 +0200

    complete mark

--- data/mark.sql
+++ data/mark.sql
@@ -1,23 +1,19 @@
 DROP TABLE IF EXISTS mark;
 CREATE VIRTUAL TABLE mark USING FTS5(chapter, verse, prefix, suffix, body);
 
-
-INSERT INTO mark (chapter, verse, prefix, suffix, body)
-VALUES (1, 1, NULL, NULL, "بَدْءُ إِنْجِيلِ يَسُوعَ الْمَسِيحِ ابْنِ اللهِ،");
-
-INSERT INTO mark (chapter, verse, prefix, suffix, body)
-VALUES (1, 2, NULL, NULL, "كَمَا هُوَ مَكْتُوبٌ فِي الأَنْبِيَاءِ: «هَا أَنَا أُرْسِلُ أَمَامَ وَجْهِكَ مَلاَكِي، الَّذِي يُهَيِّئُ طَرِيقَكَ قُدَّامَكَ.");
-
-INSERT INTO mark (chapter, verse, prefix, suffix, body)
-VALUES (1, 3, NULL, NULL, "صَوْتُ صَارِخٍ فِي الْبَرِّيَّةِ: أَعِدُّوا طَرِيقَ الرَّبِّ، اصْنَعُوا سُبُلَهُ مُسْتَقِيمَةً».");
-
-
-
-INSERT INTO mark (chapter, verse, prefix, suffix, body)
-VALUES (2, 1, NULL, NULL, "ثُمَّ دَخَلَ كَفْرَنَاحُومَ أَيْضًا بَعْدَ أَيَّامٍ، فَسُمِعَ أَنَّهُ فِي بَيْتٍ.");
-
-INSERT INTO mark (chapter, verse, prefix, suffix, body)
-VALUES (2, 2, NULL, NULL, "وَلِلْوَقْتِ اجْتَمَعَ كَثِيرُونَ حَتَّى لَمْ يَعُدْ يَسَعُ وَلاَ مَا حَوْلَ الْبَابِ. فَكَانَ يُخَاطِبُهُمْ بِالْكَلِمَةِ.");
-
-INSERT INTO mark (chapter, verse, prefix, suffix, body)
-VALUES (2, 3, NULL, NULL, "وَجَاءُوا إِلَيْهِ مُقَدِّمِينَ مَفْلُوجًا يَحْمِلُهُ أَرْبَعَةٌ.");
+.read mark/mark_1.sql
+.read mark/mark_2.sql
+.read mark/mark_3.sql
+.read mark/mark_4.sql
+.read mark/mark_5.sql
+.read mark/mark_6.sql
+.read mark/mark_7.sql
+.read mark/mark_8.sql
+.read mark/mark_9.sql
+.read mark/mark_10.sql
+.read mark/mark_11.sql
+.read mark/mark_12.sql
+.read mark/mark_13.sql
+.read mark/mark_14.sql
+.read mark/mark_15.sql
+.read mark/mark_16.sql
