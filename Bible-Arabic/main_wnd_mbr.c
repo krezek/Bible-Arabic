fa2e719a18d69e64adb1c2fe16814c2228c57109
Kinaz Rezek <kinaz_rezek@hotmail.com>
2022-11-05 07:50:39 +0100

    simple read

--- Bible-Arabic/main_wnd_mbr.c
+++ Bible-Arabic/main_wnd_mbr.c
@@ -12,6 +12,10 @@ typedef struct Parser Parser;
 void OnBtnClicked_read(MainWindow* mw, WPARAM wParam, LPARAM lParam)
 {
 	int rc = 0;
+	char buff[512];
+	short buffer[16000];
+	int i;
+	FILE* output;
 
 	rc = init_MBR(DB_MBR_URL);
 	if (rc)
@@ -20,5 +24,29 @@ void OnBtnClicked_read(MainWindow* mw, WPARAM wParam, LPARAM lParam)
 		return;
 	}
 
+	reset_MBR();
+
+	strcpy(buff, "_ 150\na 75\nb 75\na 75\nn 75\na 75\n_ 10\n");
+	rc = write_MBR(buff);
+	if (!rc)
+	{
+		ShowError(L"No enough space in the buffer");
+		close_MBR();
+		return;
+	}
+
+	flush_MBR();
+
+	output = fopen("abana.ulaw", "wb");
+
+	while ((i = readtype_MBR(buffer, 16000, LIN16)) == 16000)
+		fwrite(buffer, 2, i, output);
+
+	if (i > 0)
+	{
+		fwrite(buffer, 2, i, output);
+	}
+
+	fclose(output);
 	close_MBR();
 }
