1119b21644abd1c6490e9e85d38882ba82ce7f13
Kinaz Rezek <kinaz_rezek@hotmail.com>
2022-11-05 19:39:30 +0100

    play sound

--- Bible-Arabic/main_wnd_mbr.c
+++ Bible-Arabic/main_wnd_mbr.c
@@ -7,15 +7,20 @@ typedef struct Parser Parser;
 #include "Misc/incdll.h"
 #include "Libonechannel/onechannel.h"
 
+void write_header(int file_format, int audio_length, unsigned short samp_rate, FILE* output_file);
+
 #define DB_MBR_URL "..\\mbrola_db\\ar0"
+short buffer[16000];
 
 void OnBtnClicked_read(MainWindow* mw, WPARAM wParam, LPARAM lParam)
 {
 	int rc = 0;
 	char buff[512];
-	short buffer[16000];
 	int i;
 	FILE* output;
+	BOOL bRtn;
+	int audio_length = 0;
+	unsigned short samp_rate = 16000;
 
 	rc = init_MBR(DB_MBR_URL);
 	if (rc)
@@ -26,7 +31,7 @@ void OnBtnClicked_read(MainWindow* mw, WPARAM wParam, LPARAM lParam)
 
 	reset_MBR();
 
-	strcpy(buff, "_ 150\na 75\nb 75\na 75\nn 75\na 75\n_ 10\n");
+	strcpy(buff, "_ 150\na 75\nb 75\na 75\nn 75\na 75\n_ 150\n");
 	rc = write_MBR(buff);
 	if (!rc)
 	{
@@ -37,16 +42,28 @@ void OnBtnClicked_read(MainWindow* mw, WPARAM wParam, LPARAM lParam)
 
 	flush_MBR();
 
-	output = fopen("abana.ulaw", "wb");
-
+	output = fopen("verse.wav", "wb");
+		
 	while ((i = readtype_MBR(buffer, 16000, LIN16)) == 16000)
+	{
 		fwrite(buffer, 2, i, output);
+	}
 
 	if (i > 0)
 	{
 		fwrite(buffer, 2, i, output);
 	}
 
+	audio_length = (int)(ftell(output) / 2) - 0x16;
+	fseek(output, 0, SEEK_SET);
+	write_header(1, audio_length, samp_rate, output);
+
 	fclose(output);
 	close_MBR();
+	
+	bRtn = PlaySound(L"verse.wav", NULL, SND_FILENAME | SND_ASYNC | SND_NODEFAULT);
+	if (bRtn != TRUE)
+	{
+		ShowError(L"read wav error.");
+	}
 }
